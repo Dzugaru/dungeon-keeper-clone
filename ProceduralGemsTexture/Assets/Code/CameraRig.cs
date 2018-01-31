d2a9d45602a45620200d9cc54842ea31095ec80a
Dzugaru <dzugaru@gmail.com>
2018-01-31 22:42:09 +0300

    Added camera rig

---
+++ ProceduralGemsTexture/Assets/Code/CameraRig.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRig : MonoBehaviour
+{
+    Transform swivel, stick;
+
+    float zoom = 1;
+    float rotationAngle = 0;
+
+    public float minZoom, maxZoom, zoomSpeed, rotationSpeed;
+
+	void Start()
+    {
+        swivel = transform.Find("Swivel");
+        stick = swivel.Find("Stick");        
+    }
+	
+	void Update()
+    {
+        float zoomDelta = Input.GetAxis("Mouse ScrollWheel");
+        if (zoomDelta != 0f)
+            AdjustZoom(zoomDelta);
+
+        float rotationDelta = Input.GetAxis("Rotation");
+        if (rotationDelta != 0f)        
+            AdjustRotation(rotationDelta);        
+    }
+
+    void AdjustZoom(float delta)
+    {
+        zoom = Mathf.Clamp01(zoom + delta * zoomSpeed);
+        float distance = Mathf.Lerp(minZoom, maxZoom, zoom);
+        stick.localPosition = new Vector3(0f, 0f, distance);        
+    }
+
+    void AdjustRotation(float delta)
+    {
+        rotationAngle += delta * rotationSpeed * Time.deltaTime;
+        if (rotationAngle < 0)        
+            rotationAngle += 360f;
+        else if (rotationAngle >= 360f)        
+            rotationAngle -= 360f;
+
+        transform.localRotation = Quaternion.AngleAxis(rotationAngle, Vector3.up);
+    }
+}
